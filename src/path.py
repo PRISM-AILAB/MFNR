91e258b19a2c8b8bf085a90f77df73ac10abc7bc
Haebin Lim <fkid114@gmail.com>
2025-10-03 00:26:19 +0900

    경로 추가

---
+++ src/path.py
@@ -0,0 +1,9 @@
+import os
+
+BASE_PATH = os.getcwd()
+
+DATA_PATH = os.path.join(BASE_PATH, "data")
+PROCESSED_PATH = os.path.join(DATA_PATH, "processed")
+RAW_PATH = os.path.join(DATA_PATH, "raw")
+
+UTILS_PATH = os.path.join(BASE_PATH, "src")

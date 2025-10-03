da70098d198612c9ab4d7e47acc53da42f6347c0
Haebin Lim <fkid114@gmail.com>
2025-10-03 23:57:38 +0900

    yaml 로드, 시드 설정 추가

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
-import pandas as pd
-import gzip, json
+import pandas as pd, numpy as np
+import gzip, json, yaml, random, torch
 
 def parse(path):
   g = gzip.open(path, 'rb')
@@ -19,3 +19,13 @@ def load_parquet(fpath):
 
 def save_parquet(df: pd.DataFrame, fpath: str):
     df.to_parquet(fpath, engine = "pyarrow")
+
+def load_yaml(fpath: str) -> dict:
+   with open(fpath, "r", encoding = "utf-8") as f:
+      return yaml.safe_load(f)
+   
+def set_seed(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)

7daeac8a675d463948b66249e555c3ac97b498ae
Haebin Lim <fkid114@gmail.com>
2025-10-04 14:02:10 +0900

    메트릭 추가

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,11 @@
 import pandas as pd, numpy as np
 import gzip, json, yaml, random, torch
+from sklearn.metrics import (
+   mean_squared_error, 
+   mean_absolute_error, 
+   mean_absolute_percentage_error
+)
+
 
 def parse(path):
   g = gzip.open(path, 'rb')
@@ -29,3 +35,13 @@ def set_seed(seed: int):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+
+def get_metrics(preds, trues):
+    preds = preds.squeeze().numpy() if isinstance(preds, torch.Tensor) else preds
+    trues = trues.squeeze().numpy() if isinstance(trues, torch.Tensor) else trues
+    
+    mse = mean_squared_error(trues, preds)
+    rmse = np.sqrt(mse)
+    mae = mean_absolute_error(trues, preds)
+    mape = mean_absolute_percentage_error(trues, preds) * 100
+    return mse, rmse, mae, mape
